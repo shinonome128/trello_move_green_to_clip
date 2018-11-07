f39fcbb30d0eb489fd24f571693d317db2ece575
shinonome128 <shinonome128@gmail.com>
2018-11-07 13:57:47 +0900

    Add getting cards

--- get_card.py
+++ get_card.py
@@ -3,15 +3,13 @@
 必要モジュールロード
 """
 from trello import TrelloClient
-from pprint import pprint
-# コンフィグファイル読み込みのモジュール
 import configparser
 """
 主処理
 """
 def main():
 
-        ### 設定ファイルの読込を実行
+        # 設定ファイルの読込
         conf_file = 'conf.txt'
         config = configparser.ConfigParser()
 
@@ -35,27 +33,18 @@ def main():
         # クライアントを作成
         client = TrelloClient(API_Key, API_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-        """
-        #アカウントが作成したboard一覧を取得
-        boards = client.list_boards()
-        
-        pprint(boards)
-        """
-
-        # 全ボードをリストし変数に格納
+        # 全ボードをリスト
         all_boards = client.list_boards()
 
-        # 最後のボード名を変数に格納
-        last_board = all_boards[-1]
+        # インデクシングしてボード名を変数に格納
+        board = all_boards[1]
 
-        # 最後のボードのリストを取得
-        last_board.list_lists()
-        import pdb; pdb.set_trace()
-
-        # list_id の指定方法を調査
-        my_list = last_board.get_list(list_id)
+        # ボードの全カードを取得
+        board.all_cards()  
+        # import pdb; pdb.set_trace()
         
-        for card in my_list.list_cards():
+        # カード名を出力
+        for card in board.all_cards():
             print(card.name)
 
 """
